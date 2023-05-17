0debb9ee94bcc1bd1eed83fadbee25b24846868c
Bormit <coolxex86@gmail.com>
2023-05-17 23:45:06 +0300

    Создание дизайна фрагмента справочника животных и исправление ошибок в дизайне

--- app/src/main/java/ru/mirea/animal_habitat_monitoring_mobile/view/fragments/BottomNavigation.kt
+++ app/src/main/java/ru/mirea/animal_habitat_monitoring_mobile/view/fragments/BottomNavigation.kt
@@ -1,5 +1,6 @@
 package ru.mirea.animal_habitat_monitoring_mobile.view.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,11 +11,13 @@ import ru.mirea.animal_habitat_monitoring_mobile.R
 
 
 class BottomNavigation : Fragment() {
+    private var fragment: Int = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_bottom_navigation, container, false)
+        val menu = view.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
         if (savedInstanceState == null){
             activity?.supportFragmentManager?.beginTransaction()
@@ -22,7 +25,47 @@ class BottomNavigation : Fragment() {
                 ?.commit()
         }
 
-        val menu = view.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        when(fragment){
+            1 ->{
+                activity?.supportFragmentManager?.beginTransaction()
+                    ?.replace(R.id.containerFragment,HomeFragment())
+                    ?.commit()
+                menu.selectedItemId = R.id.homeFragment
+            }
+            2 ->{
+                activity?.supportFragmentManager?.beginTransaction()
+                    ?.replace(R.id.containerFragment,CreateOfForm())
+                    ?.commit()
+                menu.selectedItemId = R.id.listOfForm
+            }
+            3 ->{
+                activity?.supportFragmentManager?.beginTransaction()
+                    ?.replace(R.id.containerFragment,InfoOfApp())
+                    ?.commit()
+                menu.selectedItemId = R.id.infoOfApp
+            }
+            4 ->{
+                val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+                val login = sharedPref?.getString("login", "Пользователь")
+                if (login != "Пользователь"){
+                    activity?.supportFragmentManager?.beginTransaction()
+                        ?.replace(R.id.containerFragment,UserProfile())
+                        ?.commit()
+                }else{
+                    activity?.supportFragmentManager?.beginTransaction()
+                        ?.replace(R.id.containerFragment,Profile())
+                        ?.commit()
+                }
+                menu.selectedItemId = R.id.profileUser
+            }
+            5 ->{
+                activity?.supportFragmentManager?.beginTransaction()
+                    ?.replace(R.id.containerFragment,SettingsFragment())
+                    ?.commit()
+                menu.selectedItemId = R.id.settingsFragment
+            }
+        }
 
         menu.selectedItemId = R.id.homeFragment
 
@@ -33,26 +76,39 @@ class BottomNavigation : Fragment() {
                     activity?.supportFragmentManager?.beginTransaction()
                         ?.replace(R.id.containerFragment,HomeFragment())
                         ?.commit()
+                    fragment = 1
                 }
                 R.id.listOfForm -> {
                     activity?.supportFragmentManager?.beginTransaction()
                         ?.replace(R.id.containerFragment,CreateOfForm())
                         ?.commit()
+                    fragment = 2
                 }
                 R.id.infoOfApp -> {
                     activity?.supportFragmentManager?.beginTransaction()
                         ?.replace(R.id.containerFragment,InfoOfApp())
                         ?.commit()
+                    fragment = 3
                 }
                 R.id.profileUser -> {
-                    activity?.supportFragmentManager?.beginTransaction()
-                        ?.replace(R.id.containerFragment,Profile())
-                        ?.commit()
+                    val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+                    val login = sharedPref?.getString("login", "Пользователь")
+                    if (login != "Пользователь"){
+                        activity?.supportFragmentManager?.beginTransaction()
+                            ?.replace(R.id.containerFragment,UserProfile())
+                            ?.commit()
+                    }else{
+                        activity?.supportFragmentManager?.beginTransaction()
+                            ?.replace(R.id.containerFragment,Profile())
+                            ?.commit()
+                    }
+                    fragment = 4
                 }
                 R.id.settingsFragment -> {
                     activity?.supportFragmentManager?.beginTransaction()
                         ?.replace(R.id.containerFragment,SettingsFragment())
                         ?.commit()
+                    fragment = 5
                 }
             }
             true
--- app/src/main/java/ru/mirea/animal_habitat_monitoring_mobile/view/fragments/InfoOfApp.kt
+++ app/src/main/java/ru/mirea/animal_habitat_monitoring_mobile/view/fragments/InfoOfApp.kt
@@ -1,10 +1,12 @@
 package ru.mirea.animal_habitat_monitoring_mobile.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import ru.mirea.animal_habitat_monitoring_mobile.R
 
 class InfoOfApp : Fragment() {
@@ -13,6 +15,22 @@ class InfoOfApp : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_of_app, container, false)
+        val view = inflater.inflate(R.layout.fragment_info_of_app, container, false)
+
+        val infoAnimal1 = view.findViewById<TextView>(R.id.infoAnimal1)
+        val infoAnimal2 = view.findViewById<TextView>(R.id.infoAnimal2)
+        val infoAnimal3 = view.findViewById<TextView>(R.id.infoAnimal3)
+
+        infoAnimal1.text = Html.fromHtml("<h1>Ласка</h1>\n" +
+                "    <p>Ласка это небольшое млекопитающее, принадлежащее к семейству хорьковых.\n" +
+                "    Они обладают пушистым мехом, плавным движением и характерными черными полосками на лице.</p>")
+        infoAnimal2.text = Html.fromHtml( "<h2>Тигр</h2>\n" +
+                "    <p>Тигр - это крупное хищное млекопитающее, отличающееся своей мощной мускулатурой и полосатым мехом.\n" +
+                "    Они являются одними из самых крупных и сильных кошачьих.</p>")
+        infoAnimal3.text = Html.fromHtml("<h3>Слон</h3>\n" +
+                "    <p>Слон - это крупное травоядное животное, характеризующееся своими большими ушами и длинным хоботом.\n" +
+                "    Слоны считаются одними из самых умных и социальных животных.</p>")
+
+        return view
     }
 }
